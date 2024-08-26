9502e3548f3157255245f71ff78a18f916d32449
hyeyeon9037 <hyeyeon9037@naver.com>
2024-08-26 15:00:30 +0900

    20240826

--- 12.shopping-mall/src/pages/Cart.js
+++ 12.shopping-mall/src/pages/Cart.js
@@ -10,38 +10,28 @@ function Cart() {
 
     return (
         <div className='cart'>
-        <h2>{state2}님의 CART LIST </h2>
+        <br/>
+        <h2>{state2}의 CART LIST</h2>
+        <br />
         <Table striped bordered hover>
-        <br></br>
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>상품명</th>
-            <th>수량</th>
-            <th>변경하기</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-      </div>
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>상품명</th>
+                    <th>수량</th>
+                    <th>변경하기</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+          </Table>
+        </div>
     )
 }
 
